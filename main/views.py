431d1f8b5e6455b327ee9d8bd5f9208f4bcc6c93
kmo17032006 <kmo17032006@gmail.com>
2025-01-09 13:00:19 +0300

    first simple example

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from xmlrpc.client import Fault
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import context
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Main page of shop - Home',
+        'list': ['first', '2', 3],
+        'dict': {'first': 1, 'second': 2},
+        'bool': False
+        }
+    return render(request, 'main/index.html', context=context)
+
+def about(request):
+    return HttpResponse('About page')
