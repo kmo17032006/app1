b04d3b12100f6ee72cb9be99a9be6091865d856c
Oleg <84938485+KOA3103@users.noreply.github.com>
2025-02-02 01:03:29 +0300

    before Data Base

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('contact_info/', views.contact_info, name='contact_info'),
 ]
--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def index(request):
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        
+        'copyright': 'KOA3103'
         }
     return render(request, 'main/index.html', context=context)
 
@@ -15,6 +15,16 @@ def about(request):
     context = {
         'title': 'Home - About page',
         'content': 'About page',
-        'text_on_page': 'About page, About page, About page',        
+        'text_on_page': 'About page, About page, About page',
+        'copyright': 'KOA3103'
         }
     return render(request, 'main/about.html', context=context)
+
+def contact_info(request):
+    context = {
+        'title': 'Контакты',
+        'content': 'Как с нами связаться',
+        'text_on_page': 'email: kmo17032006@gmail.com, Telegram: @buypepee, Менеджер по рекламе @j_shlyapina',
+        'copyright': 'KOA3103'
+        }
+    return render(request, 'main/contact_info.html', context=context)
